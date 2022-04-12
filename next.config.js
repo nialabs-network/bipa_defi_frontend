cfd5eadd803fa9b1da6601c9274be93215fc5aed
DESKTOP-B5B81MQ\dev17 <iamalexyun@gmail.com>
2022-04-12 13:12:31 +0900

    n

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 const nextConfig = {
   reactStrictMode: false,
-  // async redirects() {
-  //   return [{ source: "/", destination: "/dashboard", permanent: true }];
-  // },
 };
 
 module.exports = nextConfig;
