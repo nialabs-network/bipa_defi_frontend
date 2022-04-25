355ab1808e175666ec47c67743b8a90cb21adcb1
DESKTOP-B5B81MQ\dev17 <iamalexyun@gmail.com>
2022-04-25 09:05:50 +0900

    swap

--- Components/Swap/Swap.jsx
+++ Components/Swap/Swap.jsx
@@ -3,9 +3,13 @@ import Button from "../Reusables/Button";
 import { useWeb3Context } from "../../Contexts";
 import { useTranslation } from "react-i18next";
 import { useState } from "react";
-import { UniswapRouter } from "../../contract/UniswapRouter";
-// import { SwapWidget } from "@uniswap/widgets";
-// import "@uniswap/widgets/fonts.css";
+
+import { SwapWidget } from "@uniswap/widgets";
+import "@uniswap/widgets/fonts.css";
+
+////////////////////////////////////////////////////////////\
+import { Pool } from "@uniswap/v3-sdk";
+////////////////////////////////////////////////////////////
 export default function Swap() {
   const { web3State } = useWeb3Context();
   const { address, balance, connect, web3Provider, provider } = web3State;
@@ -15,15 +19,8 @@ export default function Swap() {
   //////////////////////////////////////////////////////////////////////////////////////////////////////////
   ///GANACHE WETH ADDRESS = 0x32e5bEd4d487d45a3fA7233f911Ec1e0Fba2e322
   async function swap() {
-    console.log("about to swap");
-    //instantiate router
-    const router = new web3Provider.eth.Contract(
-      UniswapRouter,
-      "0xE592427A0AEce92De3Edee1F18E0157C05861564"
-    );
-    console.log(router, "uniswap router");
+    console.log();
   }
-
   //////////////////////////////////////////////////////////////////////////////////////////////////////////
   return (
     <section className={`${styles.swapContainer} glass`}>
