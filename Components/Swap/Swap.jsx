89cdfb849b53e25814cd9f3f3d7ea32363ff46e2
DESKTOP-B5B81MQ\dev17 <iamalexyun@gmail.com>
2022-04-25 17:58:46 +0900

    proxy contract solved, to do: exchange rate and swap on polygon

--- Components/Swap/Swap.jsx
+++ Components/Swap/Swap.jsx
@@ -1,58 +1,71 @@
 import styles from "./Swap.module.scss";
 import Button from "../Reusables/Button";
-import { useWeb3Context } from "../../Contexts";
+import { useAppContext, useWeb3Context } from "../../Contexts";
 import { useTranslation } from "react-i18next";
-import { useState } from "react";
-import { UniswapRouter } from "../../contract/UniswapRouter";
-// import { SwapWidget } from "@uniswap/widgets";
-// import "@uniswap/widgets/fonts.css";
+import { useState, useEffect } from "react";
+import { APP_ACTIONS } from "../../Reducers";
+import { getPrice } from "./quote";
+
 export default function Swap() {
   const { web3State } = useWeb3Context();
+  const { appState, dispatch } = useAppContext();
   const { address, balance, connect, web3Provider, provider } = web3State;
-  const [state, setState] = useState("");
-  console.log(state);
+  const [maticAmount, setMaticAmount] = useState("");
+  const [nasmgAmount, setNasmgAmount] = useState("");
   const { t } = useTranslation();
   //////////////////////////////////////////////////////////////////////////////////////////////////////////
-  ///GANACHE WETH ADDRESS = 0x32e5bEd4d487d45a3fA7233f911Ec1e0Fba2e322
-  async function swap() {
-    console.log("about to swap");
-    //instantiate router
-    const router = new web3Provider.eth.Contract(
-      UniswapRouter,
-      "0xE592427A0AEce92De3Edee1F18E0157C05861564"
-    );
-    console.log(router, "uniswap router");
-  }
 
+  async function handleSwap() {
+    dispatch({
+      type: APP_ACTIONS.SET_LOADING,
+      loading: true,
+      loading_msg: "swapping",
+    });
+    dispatch({
+      type: APP_ACTIONS.SET_LOADING,
+      loading: false,
+      loading_msg: "",
+    });
+  }
+  function handlePrice() {
+    if (maticAmount !== "") getPrice(maticAmount, web3Provider);
+  }
   //////////////////////////////////////////////////////////////////////////////////////////////////////////
   return (
     <section className={`${styles.swapContainer} glass`}>
       <h2>{t("swap")}</h2>
-      {/* {address ? (
-        <SwapWidget
-          provider={web3Provider.currentProvider}
-          jsonRpcEndpoint={provider}
-        />
-      ) : null} */}
-
       <div className={styles.input}>
         <div className={styles.token}>MATIC</div>
         <input
           type="text"
           className={styles.formInput}
-          value={state}
+          value={maticAmount}
           onChange={(e) => {
-            setState(e.target.value);
+            setMaticAmount(e.target.value);
           }}
         />
       </div>
       <div className={styles.input}>
         <div className={styles.token}>NASMG</div>
-        <input type="text" className={styles.formInput} />
+        <input
+          type="text"
+          className={styles.formInput}
+          value={nasmgAmount}
+          readOnly
+        />
       </div>
       <Button
+        value={address ? "Get Price" : "Connect wallet"}
+        onclick={address ? handlePrice : connect}
+        style={{
+          width: "100%",
+          margin: "0 auto",
+          marginTop: "2rem",
+        }}
+      />
+      <Button
         value={address ? "Swap" : "Connect wallet"}
-        onclick={address ? swap : connect}
+        onclick={address ? handleSwap : connect}
         style={{
           width: "100%",
           margin: "0 auto",
